4c639d422fcc014cdccf4153111b04d0b3a441e7
Ben <55010051+bendejong0@users.noreply.github.com>
2025-05-12 20:19:27 -0500

    Add project files.

---
+++ CS320P04DeJong.cpp
@@ -0,0 +1,14 @@
+#include "MapColor.h"
+
+using namespace std;
+
+
+int main() {
+	MapColor problemInst;
+	problemInst.getInput();
+	problemInst.solve();
+	problemInst.printOutput();
+
+
+	return 0;
+}
